23346e0ecc6fb913d4d3f7b5d101997b2f25a286
chaun14 <chaun1403@gmail.com>
2020-09-13 11:34:12 +0200

    invitelogger shard launcher

--- scripts/dev-run.js
+++ scripts/dev-run.js
@@ -1,26 +1,49 @@
 const { spawn } = require('child_process');
 
 const config = require('../config.json');
+const { Webhook } = require('discord-webhook-node');
+const hook = new Webhook("https://discordapp.com/api/webhooks/743059954809700395/2FX9m-lsdquI9-ddMDVWFf-u5xwMmDbfdaWNPFgOyJWt1BHKRFSCMfC9eQEHzcDg5DFt");
 
-let child = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['run', 'build'], {
-	stdio: 'inherit'
-});
+let totalShards = 7;
+let shards = {}
 
-const debug = process.argv[2] || false;
+for (var actualshard = 0; actualshard < totalShards + 1; actualshard++) {
+    let shard = actualshard
+    let debugport = 19400 + actualshard;
+    console.log(actualshard + " launching")
+    hook.info('**Launching**', "shard " + shard + " is launching", 'test');
 
-child.on('error', error => console.log(error));
 
-child.on('close', () => {
-	if (debug) {
-		console.log('STARTING AND WAITING FOR DEBUGGER');
-	}
+    const debug = process.argv[2] || false;
 
-	child = spawn(
-		'node',
-		[`--inspect${debug ? '-brk' : ''}=19229`, './bin/bot.js', '--no-rabbitmq', config.devToken, '1', '1'],
-		{
-			stdio: 'inherit'
-		}
-	);
-	child.on('error', error => console.log(error));
-});
+    let child = spawn(
+        'node', [`--inspect${debug ? '-brk' : ''}=${debugport}`, './bin/bot.js', '--no-rabbitmq', config.devToken, shard, totalShards], {
+            stdio: 'inherit'
+        }
+    );
+
+
+    child.on('error', error => console.log(error));
+
+
+    child.on('close', () => {
+
+        console.log('Shard ' + actualshard + 'seems to have dropped')
+        hook.error('**Error**', 'Shard ' + shard, 'seems to have dropped');
+
+
+        if (debug) {
+            console.log('STARTING AND WAITING FOR DEBUGGER');
+        }
+
+        child = spawn(
+            'node', [`--inspect${debug ? '-brk' : ''}=${debugport}`, './bin/bot.js', '--no-rabbitmq', config.devToken, shard, totalShards], {
+                stdio: 'inherit'
+            }
+        );
+
+    });
+
+
+
+}
